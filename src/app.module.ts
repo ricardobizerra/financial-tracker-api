b3edb10cecc2881fa5549dec40e88a7bcdd963cf
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-02-12 13:17:24 -0300

    feat: config redis cache module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
-import { ConfigModule } from '@nestjs/config';
-import { envSchema } from '@/env';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Env, envSchema } from '@/env';
 import { PrismaModule } from '@/lib/prisma/prisma.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
@@ -13,6 +13,8 @@ import { UserModule } from '@/user/user.module';
 import { RedisModule } from '@/lib/redis/redis.module';
 import { AuthModule } from './auth/auth.module';
 import { IpeadataModule } from './external/ipeadata/ipeadata.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -28,6 +30,16 @@ import { IpeadataModule } from './external/ipeadata/ipeadata.module';
     }),
     ConfigModule.forRoot({
       validate: (config) => envSchema.parse(config),
+      isGlobal: true,
+    }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService<Env, true>) => ({
+        store: await redisStore({
+          url: configService.get('REDIS_URL'),
+        }),
+      }),
       isGlobal: true,
     }),
     HealthModule,
