851de1c25f8f813af1fc4848316daef30c76f0a3
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-02-15 23:54:31 -0300

    feat: update select object to handle differences between database and graphql model types

--- src/utils/select-object.ts
+++ src/utils/select-object.ts
@@ -1,8 +1,52 @@
-export function selectObject<T>(
-  queriedFields: string[],
-): Partial<Record<keyof T, true>> {
-  return queriedFields.reduce(
-    (acc, field) => ({ ...acc, [field]: true }),
-    {} as Partial<Record<keyof T, true>>,
-  );
+type ExtraModelFields<TModel, TDatabase> = Exclude<
+  keyof TModel,
+  keyof TDatabase
+>;
+
+type RequiredHashFields<TModel, TDatabase> =
+  ExtraModelFields<TModel, TDatabase> extends never
+    ? Record<string, never>
+    : {
+        [K in ExtraModelFields<TModel, TDatabase>]: (keyof TDatabase)[];
+      };
+
+type SelectObjectParams<TModel, TDatabase> =
+  ExtraModelFields<TModel, TDatabase> extends never
+    ? [queriedFields: (keyof TModel)[]]
+    : [
+        queriedFields: (keyof TModel)[],
+        hashDifferentFields: RequiredHashFields<TModel, TDatabase>,
+      ];
+
+type SelectObjectReturn<TDatabase> = Partial<Record<keyof TDatabase, true>>;
+
+export function selectObject<
+  TDatabase extends Record<string, any>,
+  TModel extends Partial<TDatabase>,
+>(
+  ...args: SelectObjectParams<TModel, TDatabase>
+): SelectObjectReturn<TDatabase> {
+  const [queriedFields, hashDifferentFields] = args as SelectObjectParams<
+    TModel,
+    TDatabase
+  >;
+
+  return queriedFields.reduce((acc, field) => {
+    if (
+      !!hashDifferentFields &&
+      Object.prototype.hasOwnProperty.call(hashDifferentFields, field)
+    ) {
+      const hashedFields = hashDifferentFields[
+        field as keyof RequiredHashFields<TModel, TDatabase>
+      ] as (keyof TDatabase)[];
+
+      const hashedSelectObject = selectObject<TDatabase, TDatabase>(
+        hashedFields,
+      );
+
+      return { ...acc, ...hashedSelectObject };
+    }
+
+    return { ...acc, field: true };
+  }, {} as SelectObjectReturn<TDatabase>);
 }
