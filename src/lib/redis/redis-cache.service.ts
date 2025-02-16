80b085ab83b2b529b8b3cb760a64d65b562409d8
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-02-16 14:58:00 -0300

    feat: redis cache service with typed keys and recovery functions

---
+++ src/lib/redis/redis-cache.service.ts
@@ -0,0 +1,54 @@
+import { IpeadataService } from '@/external/ipeadata/ipeadata.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+
+type CacheKey = keyof RedisCacheService['keyFunction'];
+
+type KeyFunctionReturn<K extends CacheKey> = Awaited<
+  ReturnType<RedisCacheService['keyFunction'][K]>
+>;
+
+@Injectable()
+export class RedisCacheService {
+  private keyFunction = {
+    'external-ipeadata-cdi-daily': () => this.ipeadataService.getCdiValues(),
+    'external-ipeadata-cdi-last-date': async () =>
+      (await this.ipeadataService.getCdiValues())?.[
+        (await this.ipeadataService.getCdiValues())?.length - 1
+      ]?.VALDATA,
+  };
+
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
+    private readonly ipeadataService: IpeadataService,
+  ) {}
+
+  async get<K extends CacheKey>(key: K): Promise<KeyFunctionReturn<K> | null> {
+    const cacheValue = await this.cacheService.get<KeyFunctionReturn<K>>(key);
+
+    if (cacheValue !== null && cacheValue !== undefined) {
+      return cacheValue;
+    }
+
+    const keyFunction = this.keyFunction[key] as () => Promise<
+      KeyFunctionReturn<K>
+    >;
+
+    if (keyFunction) {
+      const value = await keyFunction();
+
+      if (value !== null && value !== undefined) {
+        await this.cacheService.set(key, value);
+      }
+
+      return value;
+    }
+
+    return null;
+  }
+
+  async set<K extends CacheKey>(key: K, value: KeyFunctionReturn<K>) {
+    return await this.cacheService.set(key, value);
+  }
+}
--- src/lib/redis/redis.module.ts
+++ src/lib/redis/redis.module.ts
@@ -1,11 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { RedisSubscriptionService } from '@/lib/redis/redis-subscription.service';
 import { ConfigModule } from '@nestjs/config';
+import { RedisCacheService } from './redis-cache.service';
+import { IpeadataModule } from '@/external/ipeadata/ipeadata.module';
 
 @Global()
 @Module({
-  imports: [ConfigModule],
-  providers: [RedisSubscriptionService],
-  exports: [RedisSubscriptionService],
+  imports: [ConfigModule, IpeadataModule],
+  providers: [RedisSubscriptionService, RedisCacheService],
+  exports: [RedisSubscriptionService, RedisCacheService],
 })
 export class RedisModule {}
