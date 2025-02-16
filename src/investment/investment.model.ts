2f4532085db9370a1ede2f5246b4e00a24e503ed
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-02-16 12:35:00 -0300

    feat: investment model

---
+++ src/investment/investment.model.ts
@@ -0,0 +1,49 @@
+import { Regime } from '@/lib/graphql/prisma-client';
+import { Ordenation } from '@/utils/args/ordenation.args';
+import { Connection } from '@/utils/models/connection.model';
+import { ArgsType, Field, Float, ID, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class InvestmentModel {
+  @Field(() => ID, { nullable: false })
+  id!: string;
+
+  @Field(() => String, { nullable: false })
+  initialAmount!: string;
+
+  @Field(() => String, { nullable: false })
+  currentAmount!: string;
+
+  @Field(() => String, { nullable: false })
+  taxedAmount!: string;
+
+  @Field(() => String, { nullable: false })
+  period!: string;
+
+  @Field(() => Int, { nullable: false })
+  duration!: number;
+
+  @Field(() => Regime, { nullable: false })
+  regimeName!: Regime;
+
+  @Field(() => Float, { nullable: false, defaultValue: 100 })
+  regimePercentage!: number;
+}
+
+@ObjectType()
+export class InvestmentConnection extends Connection(InvestmentModel) {}
+
+enum OrderBy {
+  id = 'id',
+  initialAmount = 'amount',
+  period = 'startDate',
+  duration = 'duration',
+  regimeName = 'regimeName',
+  regimePercentage = 'regimePercentage',
+}
+
+@ArgsType()
+export class OrdenationInvestmentArgs extends Ordenation(
+  InvestmentModel,
+  OrderBy,
+) {}
