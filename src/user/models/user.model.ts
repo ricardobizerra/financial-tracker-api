17aebf26cf56ee591b6312268927050eb3a916d3
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-07-18 11:45:51 -0300

    refactor: use pick type to simplify user model declaration

--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -1,21 +1,14 @@
-import { Role } from '@/lib/graphql/prisma-client';
+import { User } from '@/lib/graphql/prisma-client';
 import { Ordenation } from '@/utils/args/ordenation.args';
-import { ArgsType, Field, ID, ObjectType } from '@nestjs/graphql';
+import { ArgsType, ObjectType, PickType } from '@nestjs/graphql';
 
 @ObjectType()
-export class UserModel {
-  @Field(() => ID, { nullable: false })
-  id!: string;
-
-  @Field(() => String, { nullable: false })
-  email!: string;
-
-  @Field(() => String, { nullable: false })
-  name!: string;
-
-  @Field(() => Role, { nullable: false })
-  role!: Role;
-}
+export class UserModel extends PickType(User, [
+  'id',
+  'email',
+  'name',
+  'role',
+]) {}
 
 @ArgsType()
 export class OrdenationUserArgs extends Ordenation(UserModel) {}
