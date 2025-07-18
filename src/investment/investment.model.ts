3de2cfc8de02c1d6e5a78b3cfd1fd55dfff145be
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-07-18 11:47:54 -0300

    refactor: use omit type and database type to simplify investment model declaration

--- src/investment/investment.model.ts
+++ src/investment/investment.model.ts
@@ -1,45 +1,21 @@
-import { Regime } from '@/lib/graphql/prisma-client';
+import { Investment } from '@/lib/graphql/prisma-client';
 import { Ordenation } from '@/utils/args/ordenation.args';
 import { Connection } from '@/utils/models/connection.model';
-import { ArgsType, Field, Float, ID, Int, ObjectType } from '@nestjs/graphql';
+import { ArgsType, Field, Float, ObjectType, OmitType } from '@nestjs/graphql';
 
 @ObjectType()
-export class InvestmentModel {
-  @Field(() => ID, { nullable: false })
-  id!: string;
-
-  @Field(() => Float, { nullable: false })
-  initialAmount!: number;
-
-  @Field(() => Float, { nullable: false })
-  currentAmount!: number;
-
+export class InvestmentModel extends OmitType(Investment, [
+  'user',
+  'userId',
+] as const) {
   @Field(() => String, { nullable: false })
   currentVariation!: string;
 
   @Field(() => String, { nullable: false })
   taxPercentage!: string;
 
-  @Field(() => Float, { nullable: false })
-  taxedAmount!: number;
-
   @Field(() => String, { nullable: false })
   taxedVariation!: string;
-
-  @Field(() => String, { nullable: false })
-  period!: string;
-
-  @Field(() => String, { nullable: false })
-  finishedAt!: Date;
-
-  @Field(() => Int, { nullable: false })
-  duration!: number;
-
-  @Field(() => Regime, { nullable: false })
-  regimeName!: Regime;
-
-  @Field(() => Float, { nullable: false, defaultValue: 100 })
-  regimePercentage!: number;
 }
 
 @ObjectType()
@@ -47,12 +23,9 @@ export class InvestmentConnection extends Connection(InvestmentModel) {}
 
 @ArgsType()
 export class OrdenationInvestmentArgs extends Ordenation(InvestmentModel, [
-  'currentAmount',
-  'taxedAmount',
   'currentVariation',
   'taxedVariation',
   'taxPercentage',
-  'finishedAt',
 ]) {}
 
 @ObjectType()
