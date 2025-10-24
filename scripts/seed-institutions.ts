5f837c5bb0a75c23fd8bfcccff2626155f63dab8
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-10-23 22:52:35 -0300

    feat(account): seed institutions via pluggy api

---
+++ scripts/seed-institutions.ts
@@ -0,0 +1,80 @@
+import { PrismaClient } from '@prisma/client';
+
+interface Institution {
+  id: number;
+  name: string;
+  imageUrl: string;
+  primaryColor: string;
+}
+
+const prisma = new PrismaClient();
+
+async function createApiKey(): Promise<string> {
+  const response = await fetch('https://api.pluggy.ai/auth', {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      clientId: process.env.PLUGGY_CLIENT_ID,
+      clientSecret: process.env.PLUGGY_CLIENT_SECRET,
+    }),
+  });
+
+  const data = await response.json();
+
+  return data.apiKey;
+}
+
+async function fetchInstitutions(apiKey: string): Promise<Institution[]> {
+  const response = await fetch(
+    'https://api.pluggy.ai/connectors?countries=BR',
+    {
+      headers: {
+        'X-API-KEY': apiKey,
+        accept: 'application/json',
+      },
+    },
+  );
+
+  const data = await response.json();
+  return data.results as Institution[];
+}
+
+async function seedInstitutions() {
+  const apiKey = await createApiKey();
+  const institutions = await fetchInstitutions(apiKey);
+
+  await Promise.all(
+    institutions.map((institution) =>
+      prisma.institution.upsert({
+        where: {
+          code: institution.id.toString(),
+        },
+        create: {
+          name: institution.name,
+          code: institution.id.toString(),
+          logoUrl: institution.imageUrl,
+          color: institution.primaryColor,
+        },
+        update: {
+          name: institution.name,
+          code: institution.id.toString(),
+          logoUrl: institution.imageUrl,
+          color: institution.primaryColor,
+        },
+      }),
+    ),
+  );
+}
+
+seedInstitutions()
+  .then(() => {
+    console.log('📊 Institutions seeded successfully');
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error('⛔ Error seeding institutions:', e);
+    process.exit(1);
+  });
