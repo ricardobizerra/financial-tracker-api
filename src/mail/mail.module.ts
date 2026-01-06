fbf132bd2b9af957a9ba306968e3327740b809db
ricardobizerra <ricardo.bizerra19@gmail.com>
2026-01-06 18:50:52 -0300

    fix: validate password reset tokens in mail processor

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
+import { JwtModule } from '@nestjs/jwt';
 import { MailService } from './mail.service';
 import { MailProcessor } from './mail.processor';
 import { MAIL_QUEUE } from './mail.constants';
 
 @Module({
-  imports: [BullModule.registerQueue({ name: MAIL_QUEUE })],
+  imports: [BullModule.registerQueue({ name: MAIL_QUEUE }), JwtModule],
   providers: [MailService, MailProcessor],
   exports: [MailService],
 })
