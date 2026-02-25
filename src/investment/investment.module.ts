5ed2a6b9e7832740a420b91e818aa11559f4623a
ricardobizerra <ricardo.bizerra19@gmail.com>
2026-02-24 23:28:44 -0300

    fix: import institution connection module on investment module

--- src/investment/investment.module.ts
+++ src/investment/investment.module.ts
@@ -5,9 +5,16 @@ import { IpeadataModule } from '@/external/ipeadata/ipeadata.module';
 import { BacenModule } from '@/external/bacen/bacen.module';
 import { AccountModule } from '@/account/account.module';
 import { TransactionModule } from '@/transaction/transaction.module';
+import { InstitutionConnectionModule } from '@/institution-connection/institution-connection.module';
 
 @Module({
-  imports: [IpeadataModule, BacenModule, AccountModule, TransactionModule],
+  imports: [
+    IpeadataModule,
+    BacenModule,
+    AccountModule,
+    TransactionModule,
+    InstitutionConnectionModule,
+  ],
   providers: [InvestmentResolver, InvestmentService],
 })
 export class InvestmentModule {}
